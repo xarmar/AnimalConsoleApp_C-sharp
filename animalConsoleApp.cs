8df7ed09ea58548dbd0af326c8857bb852ee8fc7
Xavier Marques <xavierpmarques@gmail.com>
2021-06-16 17:05:56 +0100

    add Options to user. User can add new animals, view previously added animals and quit program.

--- animalConsoleApp.cs
+++ animalConsoleApp.cs
@@ -18,20 +18,25 @@ namespace animalConsoleApp
         }
 
         public override string ToString() {
-            string obj = $"Parabéns tem um {this.especie} da cor {this.cor} com porte {this.porte}!";
+            string obj = $"\nParabéns tem um {this.especie} da cor {this.cor} com porte {this.porte}!";
             return obj;
         }
 
         static public List <Animal> animalList = new List<Animal>();
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Bem-vindo/a. Esta aplicação irá imprimir os detalhes de um animal introduzido à sua escolha.");
+        public static void mainProgram() {
             // Create a tuple with 3 items: Item1 = Especie, Item2 = Cor, Item3 = Porte.
             var tuple = HelperFunction.getAnimalDetails();
             Animal newAnimal = new Animal(tuple.Item1, tuple.Item2, tuple.Item3);
-            Console.WriteLine(newAnimal.ToString());
             animalList.Add(newAnimal);
+            Console.WriteLine(newAnimal.ToString());
+            HelperFunction.giveUserOptions();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo/a. Esta aplicação irá imprimir os detalhes de um animal introduzido à sua escolha.");
+            mainProgram();
         }
     }
 
@@ -39,15 +44,15 @@ namespace animalConsoleApp
 
         // Runs getSpecies(), getColor() & getSize() and returns values in a Tuple.
         public static Tuple<string,string,string> getAnimalDetails() {
-            string especie = HelperFunction.getSpecies();
-            string cor = HelperFunction.getColor();
-            string porte = HelperFunction.getSize();
+            string especie = getSpecies();
+            string cor = getColor();
+            string porte = getSize();
             return new Tuple <string, string, string>(especie, cor, porte);
         }
         
         // Prompt user for the "especie" of the animal
         public static string getSpecies() {
-            Console.WriteLine("Por favor, introduza a espécie do animal: ");
+            Console.WriteLine("\n-> Por favor, introduza a espécie do animal: ");
             string especie = Console.ReadLine();
             // validate input
             if (especie != null && especie != "" && stringOnlyContainsAlphabeticalChars(especie)) {
@@ -60,7 +65,7 @@ namespace animalConsoleApp
         
         // Prompt user for the "color" of the animal
         public static string getColor() {
-            Console.WriteLine("Por favor, introduza a cor do animal: ");
+            Console.WriteLine("-> Por favor, introduza a cor do animal: ");
             string cor = Console.ReadLine(); 
             // validate input
             if (cor != null && cor != "" && stringOnlyContainsAlphabeticalChars(cor)) {
@@ -73,7 +78,7 @@ namespace animalConsoleApp
         
         // Prompt user for the 'weight' of the animal, return 'porte'
         public static string getSize() {
-            Console.WriteLine("Por favor, introduza o peso do animal (em kg): ");
+            Console.WriteLine("-> Por favor, introduza o peso do animal (em kg): ");
             string inputString = Console.ReadLine();
             if (inputString != null && inputString != "" && inputString != "0" && stringOnlyContainsNumbers(inputString)) {
                 float peso = float.Parse(inputString);
@@ -109,7 +114,7 @@ namespace animalConsoleApp
             }
         }
 
-        // Validate Input - Check if string only has alphabetic characters.
+        // Validate user input - Check if string only has alphabetic characters.
         public static Boolean stringOnlyContainsAlphabeticalChars(string stringToValidate) {
             for (var i = 0; i < stringToValidate.Length; i++) {
                 var currentChar = stringToValidate[i];
@@ -121,6 +126,7 @@ namespace animalConsoleApp
             return true;
         }
 
+        // Validate user input - Check if string only has numbers.
         public static Boolean stringOnlyContainsNumbers(string stringToValidate) {
             Regex rx = new Regex(@"^([0-9][0-9]*)(\.[1-9]+)?$");
             if (rx.IsMatch(stringToValidate)) {
@@ -130,8 +136,47 @@ namespace animalConsoleApp
             return false;
         }
 
+        // program loop
+        public static void giveUserOptions() {
+            printIntructions();
+            ConsoleKeyInfo keyInfo;
+            do {
+                keyInfo = Console.ReadKey();
+            } while (!userPressedOneOfTheOptions(keyInfo));
+            }
+
+        public static void printIntructions () {
+            Console.WriteLine("\n Escolha uma opção (pressione no seu teclado):");
+            Console.WriteLine("   1. Adicionar outro animal. ");
+            Console.WriteLine("   2. Ver animais adicionados. ");
+            Console.WriteLine("   3. Sair do programa. "); 
+        }
+
+        public static bool userPressedOneOfTheOptions (ConsoleKeyInfo keyInfo) {
+            if (keyInfo.Key == ConsoleKey.D1) {
+                Animal.mainProgram();
+                return true;
+            }
+            if (keyInfo.Key == ConsoleKey.D2) {
+                printAllAnimals();
+                giveUserOptions();
+                return true;
+            }
+            if (keyInfo.Key == ConsoleKey.D3) {
+                return true;
+            }
+
+            else {
+                Console.WriteLine("\n ---------- INVÁLIDO ----------");
+                printIntructions();
+                return false;
+            }
+        }
+
         // Print all animals in List
         public static void printAllAnimals() {
+            Console.WriteLine("\n Aqui estão os seus animais previamente adicionados:");
+
             foreach( Animal animal in Animal.animalList) {
                     Console.WriteLine(animal.ToString());
                 }
